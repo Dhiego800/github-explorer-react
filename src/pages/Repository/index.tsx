1ee13d559534bac1cd938f3553eba058f4c65835
unknown <diiego.mm@hotmail.com>
2020-06-28 20:04:11 -0300

    Listando issues da API [App Concluído]

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,57 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-import logoImg from '../../assets/logo.svg';
 import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
 import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
 };
+
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  }
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setissues] = useState<issue[]>([]);
+
   const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      setRepository(response.data);
+    });
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      setissues(response.data);
+    })
+  },[params.repository]);
 
 return (
   <>
     <Header>
-      <img src={logoImg} alt="Github Explorer" />
+      <img 
+        src={logoImg} 
+        alt="Github Explorer" />
       <Link to="/">
         <FiChevronsLeft size={16} />
         Voltar
       </Link>
     </Header>
 
-    <RepositoryInfo>
+    {repository && (
+      <RepositoryInfo>
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/48865946?s=460" alt="Avatar" />
+        <img 
+          src={repository.owner.avatar_url} 
+          alt={repository.owner.login} />
         <div>
-          <strong>Repositorio/Repositorio</strong>
-          <p>descrição descrição jaajkajkkak</p>
+          <strong>{repository.full_name}</strong>
+          <p>{repository.description}</p>
         </div>
       </header>
       <ul>
         <li>
-          <strong>1808</strong>
+          <strong>{repository.stargazers_count}</strong>
           <span>Stars</span>
         </li>
         <li>
-          <strong>48</strong>
+          <strong>{repository.forks_count}</strong>
           <span>Forks</span>
         </li>
         <li>
-          <strong>67</strong>
+          <strong>{repository.open_issues_count}</strong>
           <span>Issues Abertos</span>
         </li>            
       </ul>
     </RepositoryInfo>
-
+    )}
     <Issues>
-      <Link to={`asas`}>
+      {issues.map(issue => (
+      <a key={issue.id} href={issue.html_url}>
           <div>
-            <strong>sadddsadsa</strong>
-            <p>sdadadsasaddsa</p>
+            <strong>{issue.title}</strong>
+            <p>{issue.user.login}</p>
           </div>
           <FiChevronRight size={20} />
-        </Link>
+        </a>))}
     </Issues>
   </>
   );
